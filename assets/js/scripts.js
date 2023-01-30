2fa464a81e5fbf16b5ca742e463cedf30171286b
bersekiel <exebarco280320@gmail.com>
2023-01-30 02:35:30 -0300

    [EDIT] Cambios al CSS y Front

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,23 +5,21 @@ const Encriptar = document.querySelector("#btnencriptar");
 const Desencriptar = document.querySelector("#btndesencriptar");
 const copiar = document.querySelector("#btncopiar");
 
-function encriptar (){
+function encriptar() {
     let texto = textoingresado.value;
     let textoEncriptado = texto
-    .replaceAll("e", "enter")
-    .replaceAll("i", "imes")
-    .replaceAll("o", "ober")
-    .replaceAll("a", "ai")
-    .replaceAll("u", "ufat");
+        .replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("o", "ober")
+        .replaceAll("a", "ai")
+        .replaceAll("u", "ufat");
 
-    
-   textosaliente.value = textoEncriptado;
 
-   
+    textosaliente.value = textoEncriptado;
+
+
 
 }
 
 
-btnencriptar.onclick= encriptar;
-
-b
+btnencriptar.onclick = encriptar;
