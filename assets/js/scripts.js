f324f0757b241ad4e6865eb6e1939c28b3833dd4
jesus <elpatagaming@gmail.com>
2023-01-30 16:29:35 -0500

    creando el sistema logico y modificando parte visual de los contenedores

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,20 +6,46 @@ const Desencriptar = document.querySelector("#btndesencriptar");
 const copiar = document.querySelector("#btncopiar");
 
 function encriptar() {
-    let texto = textoingresado.value;
-    let textoEncriptado = texto
-        .replaceAll("e", "enter")
-        .replaceAll("i", "imes")
-        .replaceAll("o", "ober")
-        .replaceAll("a", "ai")
-        .replaceAll("u", "ufat");
+  let textoEntrada = textoingresado.value;
+  let textoEncriptado = textoEntrada
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("o", "ober")
+    .replaceAll("a", "ai")
+    .replaceAll("u", "ufat");
 
+  document.getElementById("imagensalida").style.display = "none";
+  document.getElementById("textos").style.display = "none";
+  document.getElementById("btncopiar").style.visibility = "visible";
+  document.getElementById("textoSalida").style.display = "block";
 
-    textosaliente.value = textoEncriptado;
+  textosaliente.value = textoEncriptado;
 
-
-
+  document.getElementById("textoEntrada").value = "";
 }
 
+function desencriptar() {
+  let textoEncriptado = textoingresado.value;
+  let textoEntrada = textoEncriptado
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ober", "o")
+    .replaceAll("ai", "a")
+    .replaceAll("ufat", "u");
 
+  document.getElementById("imagensalida").style.display = "none";
+  document.getElementById("textos").style.display = "none";
+  document.getElementById("btncopiar").style.visibility = "visible";
+  document.getElementById("textoSalida").style.display = "block";
+
+  textosaliente.value = textoEntrada;
+
+  document.getElementById("textoEntrada").value = "";
+}
+function copiartexto() {
+  let textoEncriptado = textosaliente.value;
+  navigator.clipboard.writeText(textoEncriptado);
+}
 btnencriptar.onclick = encriptar;
+btndesencriptar.onclick = desencriptar;
+btncopiar.onclick = copiartexto;
